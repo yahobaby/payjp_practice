f978bf21e9ad5df1eefa17bf08e4a38b8db4eb95
yahobaby <hellotechcamp@gmail.com>
2023-01-07 12:12:24 +0900

    payjp from v1 to v2 API reset test

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+2023 / 1 / 6
+既存PAYJPのV1API終了に伴い、V2APIへ切り直す必要があり。
+練習の為、ファイルです。
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,4 +1,37 @@
 const pay = () => {
+  //環境変数をもとに公開鍵を呼び出す
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY); 
+  //elementsインスタンスを生成
+  const elements = payjp.elements();
+  //入力欄ごとにelementインスタンスを生成
+  const numberElement = elements.create('cardNumber')
+  const cvcElement = elements.create('cardCvc')
+  const expiryElement = elements.create('cardExpiry')
+  //入力欄をブラウザ上に表示
+  numberElement.mount('#number')
+  cvcElement.mount('#cvc') 
+  expiryElement.mount('#exp-date')
+  //フォームの要素を取得
+  const form = document.getElementById("charge-form");
+  //PAY.JPと通信が成功した場合のみトークンをフォームに埋め込む
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    payjp.createToken(expiryElement).then((response) => {
+      if (response.error) {
+      }
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form"); 
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        document.getElementById("charge-form").submit();
+    });
+  });
+};
+
+window.addEventListener("load", pay);
+
+/* v1からv2へ変更となった為、部分修正20230106
+const pay = () => {
   //Payjp.setPublicKeyというオブジェクトとメソッドは、導入したpayjp.jsの中で定義されているもの
   // Payjp.setPublicKey("pk_test_261d031e025e6da409352989"); // PAY.JPテスト公開鍵
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);//環境変数を呼び出すため
@@ -79,3 +112,5 @@ const pay = () => {
 };
 
 window.addEventListener("load", pay);
+
+*/
